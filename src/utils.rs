9393f4f4f41f850215644a3741fac990110b2b77
Nancy Chen <nancy@routific.com>
2019-12-05 11:54:35 -0800

    Update goauth dep to 0.6

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,11 @@
 use rustc_serialize::base64::{ToBase64, STANDARD};
 
 use goauth::auth::{JwtClaims, Token};
-use goauth::scopes::Scope;
-use goauth::get_token_with_creds;
 use goauth::credentials::Credentials;
+use goauth::get_token_with_creds;
+use goauth::scopes::Scope;
 use smpl_jwt::Jwt;
+use std::str::FromStr;
 
 use error::BTErr;
 
@@ -21,10 +22,13 @@ pub fn get_auth_token(c: &str, fp: bool) -> Result<Token, BTErr> {
         Credentials::from_str(c)?
     };
 
-    let claims = JwtClaims::new(credentials.iss(),
-                             Scope::CloudPlatform,
-                             credentials.token_uri(),
-                             None, Some(60));
+    let claims = JwtClaims::new(
+        credentials.iss(),
+        &Scope::CloudPlatform,
+        credentials.token_uri(),
+        None,
+        Some(60),
+    );
     let jwt = Jwt::new(claims, credentials.rsa_key()?, None);
     Ok(get_token_with_creds(&jwt, &credentials)?)
 }
@@ -33,5 +37,4 @@ pub fn row_key_from_str(str: &str) -> Vec<u8> {
     let mut row_key = Vec::new();
     row_key.extend_from_slice(str.as_bytes());
     row_key
-
 }
